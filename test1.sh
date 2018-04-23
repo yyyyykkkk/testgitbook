fab4f804e9bf77714ac47419a3839eb2c5d09102
qiuzhaojin <zhaojin.qiu@rayjoy.com>
2018-04-23 22:03:39 +0800

    vvc

--- test1.sh
+++ test1.sh
@@ -3,3 +3,5 @@ cd $_bookPath
 gitbook init
 gitbook install
 gitbook build 
+
+rsync -avz --progress --no-perms --omit-dir-times   /godata/pipelines/agent1_pipeline1/_book/gitbook/ rayjoy@119.28.5.63:/opt/dockerTest/gocdtest/testgitbook/gitbook/ --exclude='.git' --exclude='.DS_Store' --exclude='rsdk_ios_*' --exclude='iOS_*' --exclude='rsdk_iOS_*' --exclude='cruise-output'   -e "ssh -p 10220"
