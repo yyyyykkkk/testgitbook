a1e01b9e640eddffacc5815d3ab3ff3e6fde9a2a
qiuzhaojin <zhaojin.qiu@rayjoy.com>
2018-04-23 21:46:49 +0800

    mm

--- test1.sh
+++ test1.sh
@@ -3,7 +3,3 @@ cd $_bookPath
 gitbook init
 gitbook install
 gitbook build 
-
-rsync -avz --progress --no-perms --omit-dir-times   /godata/pipelines/agent1_pipeline1/_book/gitbook/ rayjoy@119.28.5.63:/opt/dockerTest/gocdtest/testgitbook/gitbook/ --exclude='.git' --exclude='.DS_Store' --exclude='rsdk_ios_*' --exclude='iOS_*' --exclude='rsdk_iOS_*' --exclude='cruise-output'   -e "ssh -p 10220"
-
-#rsync -avz --progress --no-perms --omit-dir-times  --delete /godata/pipelines/agent1_pipeline1/chapter1/ rayjoy@119.28.5.63:/opt/dockerTest/gocdtest/testgitbook/chapter1/ --exclude='.git' --exclude='.DS_Store' --exclude='rsdk_ios_*' --exclude='iOS_*' --exclude='rsdk_iOS_*' --exclude='cruise-output'   -e "ssh -p 10220"
